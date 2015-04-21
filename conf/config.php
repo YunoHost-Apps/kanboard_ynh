458a188199acceb5a70904082cb3448bac7411e7
mbugeia <maxime.bugeia@gmail.com>
2015-04-21 17:56:32 +0200

    Update config.php

--- conf/config.php
+++ conf/config.php
@@ -40,6 +40,9 @@ define('DB_HOSTNAME', 'localhost');
 // Mysql/Postgres database name
 define('DB_NAME', 'yuno_dbuser');
 
+// Mysql/Postgres custom port (null = default port)
+define('DB_PORT', null);
+
 // Enable LDAP authentication (false by default)
 define('LDAP_AUTH', false);
 
@@ -121,3 +124,9 @@ define('REVERSE_PROXY_DEFAULT_DOMAIN', 'yuno_domain');
 
 // Enable or disable "Strict-Transport-Security" HTTP header
 define('ENABLE_HSTS', true);
+
+// Enable or disable "X-Frame-Options: DENY" HTTP header
+define('ENABLE_XFRAME', true);
+
+// Escape html inside markdown text
+define('MARKDOWN_ESCAPE_HTML', true);
