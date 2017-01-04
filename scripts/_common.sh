97502804a5708fe3f79dad7a93eb700a08515dbc
Jimmy Monin <jimmy@monin.net>
2017-01-04 22:29:11 +0100

    Take review into account

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -16,11 +16,13 @@ APPLICATION_SOURCE_URL="https://kanboard.net/kanboard-${VERSION}.zip"
 # usage: extract_application_to DESTDIR
 extract_application() {
   local DESTDIR=$1
-  archive="${DESTDIR}/application.zip"
+  TMPDIR=$(mktemp -d)
+  chmod 755 $TMPDIR
+  archive="${TMPDIR}/application.zip"
   wget -q -O "$archive" "$APPLICATION_SOURCE_URL" \
     || ynh_die "Unable to download application archive"
-  # Here we process with unzip as would tar "--strip-component" option
-  unzip -qq "$archive" -d "$DESTDIR" && rm "$archive" && f=("$DESTDIR"/*) && mv "$DESTDIR"/*/* "$DESTDIR" && rm -f "${f[@]}"/.htaccess && rmdir "${f[@]}" \
+  unzip -qq "$archive" -d "$TMPDIR" \
     || ynh_die "Unable to extract application archive"
-  chmod 755 $DESTDIR
+  rm "$archive"
+  sudo rsync -a "$TMPDIR"/*/* "$DESTDIR"
 }
