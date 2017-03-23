770b008b746be36b334abe68df47d9ea2f55c829
Jimmy Monin <jimmy@monin.net>
2017-03-23 22:33:27 +0100

    Run migrations during upgrade script (closes #42)

--- conf/config.php
+++ conf/config.php
@@ -52,7 +52,7 @@ define('MAIL_SENDMAIL_COMMAND', '/usr/sbin/sendmail -bs');
 // Run automatically database migrations
 // If set to false, you will have to run manually the SQL migrations from the CLI during the next Kanboard upgrade
 // Do not run the migrations from multiple processes at the same time (example: web page + background worker)
-define('DB_RUN_MIGRATIONS', true);
+define('DB_RUN_MIGRATIONS', false);
 
 // Database driver: sqlite, mysql or postgres (sqlite by default)
 define('DB_DRIVER', 'mysql');
