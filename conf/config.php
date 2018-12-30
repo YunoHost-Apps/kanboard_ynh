6b34cb9332370c0e51911673f324581075e64756
Jimmy Monin <jimmy@monin.net>
2018-12-30 21:09:28 +0100

    Upgrade to upstream version 1.2.7

--- conf/config.php
+++ conf/config.php
@@ -13,7 +13,7 @@ define('LOG_DRIVER', '');
 define('LOG_FILE', DATA_DIR.DIRECTORY_SEPARATOR.'debug.log');
 
 // Plugins directory
-define('PLUGINS_DIR', 'plugins');
+define('PLUGINS_DIR', __DIR__.DIRECTORY_SEPARATOR.'plugins');
 
 // Plugins directory URL
 define('PLUGIN_API_URL', 'https://kanboard.net/plugins.json');
