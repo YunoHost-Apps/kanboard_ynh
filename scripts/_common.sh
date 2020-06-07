9044d51e939ad6b95b96e5ed17bb17caf7585109
Jimmy Monin <jimmy@monin.net>
2020-06-07 19:20:26 +0200

    Upgrade to upstream version 1.2.14 / use PHP 7.3

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,5 +3,5 @@
 #=================================================
 # COMMON VARIABLES
 #=================================================
-
-pkg_dependencies="php-gd php-zip php-dom php-mbstring"
+YNH_PHP_VERSION="7.3"
+extra_php_dependencies="php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-dom php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-mysql"
